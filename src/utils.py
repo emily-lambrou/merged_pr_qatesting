b8adbd76bfd9d0680e3ab20c4c74cbe380762b18
Emily <emily-l93@hotmail.com>
2024-09-16 15:30:54 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,23 +2,6 @@ import config
 from logger import logger
 import graphql
 
-def prepare_issue_comment(issue: dict, assignees: dict):
-    """
-    Prepare the comment from the given arguments and return it
-    """
-
-    comment = ''
-    if assignees:
-        for assignee in assignees:
-            comment += f'@{assignee["login"]} '
-    else:
-        logger.info(f'No assignees found for issue #{issue["number"]}')
-
-    comment += f'This issue is ready for testing. Please proceed accordingly.'
-    logger.info(f'Issue {issue["title"]} | {comment}')
-
-    return comment
-
 
 def check_comment_exists(issue_id, comment_text):
     """Check if the comment already exists on the issue."""
