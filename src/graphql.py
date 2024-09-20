3992df5b7f68de943b4a9f35bc8a4c54334cc3c5
Emily <emily-l93@hotmail.com>
2024-09-20 13:42:02 +0300

    Update graphql.py

--- src/graphql.py
+++ src/graphql.py
@@ -193,6 +193,97 @@ def get_project_issues(owner, owner_type, project_number, status_field_name, fil
         logging.error(f"Request error: {e}")
         return []
 
+def get_project_items(owner, owner_type, project_number, status_field_name, filters=None, after=None, items=None):
+    query = f"""
+    query GetProjectItems($owner: String!, $projectNumber: Int!, $status: String!, $after: String) {{
+      {owner_type}(login: $owner) {{
+        projectV2(number: $projectNumber) {{
+          id
+          title
+          items(first: 100, after: $after) {{
+            nodes {{
+              id
+              fieldValueByName(name: $status) {{
+                ... on ProjectV2ItemFieldSingleSelectValue {{
+                  id
+                  name
+                }}
+              }}
+              content {{
+                ... on Issue {{
+                  id
+                  title
+                  state
+                  url
+                  assignees(first: 10) {{
+                    nodes {{
+                      name
+                      email
+                      login
+                    }}
+                  }}
+                }}
+              }}
+            }}
+            pageInfo {{
+              endCursor
+              hasNextPage
+            }}
+          }}
+        }}
+      }}
+    }}
+    """
+
+    
+    variables = {
+        'owner': owner,
+        'projectNumber': project_number,
+        'status': status_field_name,
+        'after': after
+    }
+
+    try:
+        response = requests.post(
+            config.api_endpoint,
+            json={"query": query, "variables": variables},
+            headers={"Authorization": f"Bearer {config.gh_token}"}
+        )
+
+        data = response.json()
+
+        if 'errors' in data:
+            logging.error(f"GraphQL query errors: {data['errors']}")
+            return []
+
+        owner_data = data.get('data', {}).get(owner_type, {})
+        project_data = owner_data.get('projectV2', {})
+        items_data = project_data.get('items', {})
+        pageinfo = items_data.get('pageInfo', {})
+        nodes = items_data.get('nodes', [])
+
+        if items is None:
+            items = []
+
+        items += nodes
+
+        if pageinfo.get('hasNextPage'):
+            return get_project_items(
+                owner=owner,
+                owner_type=owner_type,
+                project_number=project_number,
+                status_field_name=status_field_name,
+                after=pageinfo.get('endCursor'),
+                items=items
+            )
+
+        return items
+
+    except requests.RequestException as e:
+        logging.error(f"Request error: {e}")
+        return []
+
+
 def get_project_id_by_title(owner, project_title):
     query = """
     query($owner: String!, $projectTitle: String!) {
@@ -296,7 +387,6 @@ def get_status_field_id(project_id, status_field_name):
         logging.error(f"Request error: {e}")
         return None
 
-
 def get_item_id_by_issue_id(project_id, issue_id):
     query = """
     query($projectId: ID!) {
@@ -330,19 +420,17 @@ def get_item_id_by_issue_id(project_id, issue_id):
         data = response.json()
         project_items = data.get('data', {}).get('node', {}).get('items', {}).get('nodes', [])
         
-        # Iterate over the project items to find the matching `issue_id`
         for item in project_items:
-            # Check if 'content' exists and is an Issue with an 'id'
             if item.get('content') and item['content'].get('id') == issue_id:
                 return item['id']
         
-        # If no matching issue_id is found, return None
         return None
         
     except requests.RequestException as e:
         logging.error(f"Request error: {e}")
         return None
-        
+
+
 def get_issue_has_merged_pr(issue_id):
     query = """
     query GetIssueTimeline($issueId: ID!, $afterCursor: String) {
@@ -425,8 +513,6 @@ def get_issue_has_merged_pr(issue_id):
         logging.error(f"Request error: {e}")
         return False
 
-
-
 def update_issue_status_to_qa_testing(owner, project_title, project_id, status_field_id, item_id, status_option_id):
     mutation = """
     mutation UpdateIssueStatus($projectId: ID!, $itemId: ID!, $statusFieldId: ID!, $statusOptionId: ID!) {
@@ -464,3 +550,5 @@ def update_issue_status_to_qa_testing(owner, project_title, project_id, status_f
     except requests.RequestException as e:
         logging.error(f"Request error: {e}")
         return None
+
+
